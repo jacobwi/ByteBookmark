86f33b50de17ca53bbd51d37477489ed21d54fbe
Jacob William <me@jacobwi.net>
2024-03-12 19:09:26 -0400

    Update linting configuration and add eslint dependency

---
+++ .eslintrc.cjs
@@ -0,0 +1,33 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:prettier/recommended",
+  ],
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  parser: "@typescript-eslint/parser",
+  plugins: ["react-refresh"],
+  rules: {
+    "react-refresh/only-export-components": [
+      "warn",
+      { allowConstantExport: true },
+    ],
+    "prettier/prettier": [
+      "error",
+      {
+        semi: true,
+        singleQuote: true,
+        trailingComma: "es5",
+        bracketSpacing: true,
+        jsxBracketSameLine: false,
+        printWidth: 80,
+        tabWidth: 2,
+        useTabs: false,
+        arrowParens: "avoid",
+      },
+    ],
+  },
+};
