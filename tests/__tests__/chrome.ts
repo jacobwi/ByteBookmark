e7ac047f11875def37b8e3784785f3fd48cc0773
jacobwi <me@jacobwi.net>
2024-04-03 02:35:44 -0400

    Update project configuration and dependencies

--- tests/__tests__/chrome.ts
+++
@@ -1,6 +0,0 @@
-// generate mock for chrome / browser APIs jest
-describe('Chrome Extension Tests', () => {
-    it('should pass a dummy test', () => {
-      expect(true).toBe(true);
-    });
-  });
